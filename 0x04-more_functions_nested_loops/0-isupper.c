fe7d15a8bc334057429d216b10a9b7746cc70c59
Chustar-alt <mzukise17@gmail.com>
2022-07-07 15:01:25 -0700

    Checks function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include"main.h"
+include<stdio.h>
+
+/**
+ *  main - checks alphabet code
+ *
+ *  Return: 1 if true
+ */
+
+int _isupper(int c)
+{ 
+	if (c >= 65 && c <= 90)
+	{ return(1);
+	}
+	{ return(0);
+	}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include"main.h"
+
+/**
+ * _isdigit - checks for a digit (between 0 and 9)
+ * @c: int to be checked
+ * Return: 1 if int is a digit, else
+ */
+int _isdigit(int c)
+{ 
+return(c >= '0' && c <= '9');
